552a55785300bf2d65acd890d8200f80ccde5169
Marc Wesley <marcwesley99@googlemail.com>
2015-01-23 15:34:12 +0000

    Controller support
    Added controller support so you can aim and shoot the gelgun with the
    right analog stick and right trigger

--- Assets/GelGun.cs
+++ Assets/GelGun.cs
@@ -8,6 +8,9 @@ public class GelGun : MonoBehaviour {
     public GameObject gelBit;
     private Vector2 shootDir;
     public float shootSpeed;
+    public bool controller;
+    private float x;
+    private float y;
 
 	// Use this for initialization
 	void Start () {
@@ -16,11 +19,21 @@ public class GelGun : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log(mousePos.magnitude);
-        z = Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x)) * Mathf.Rad2Deg;
-        transform.eulerAngles = new Vector3(0, 0, z);
 
+        if (!controller)
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //Debug.Log(mousePos.magnitude);
+            z = Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x)) * Mathf.Rad2Deg;     
+        }
+        else if (controller)
+        {
+            x = Input.GetAxis("RightX");
+            y = Input.GetAxis("RightY");
+            z = Mathf.Atan2(-y, x) * Mathf.Rad2Deg;
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, z);
         ShootGel();
 
 	
@@ -28,10 +41,17 @@ public class GelGun : MonoBehaviour {
 
     private void ShootGel()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButton("Fire1") || Input.GetAxis("Right Trigger") != 0)
         {
-            float wobbleFactor = Random.Range(-2.0f, 2.0f);
-            shootDir = mousePos - transform.position;
+            float wobbleFactor = Random.Range(-1.0f, 1.0f);
+            if (!controller)
+            {
+                shootDir = mousePos - transform.position;
+            }
+            else if (controller)
+            {
+                shootDir = new Vector2(x, -y);
+            }
             GameObject gelbit = Instantiate(gelBit, transform.position, Quaternion.identity) as GameObject;
             Vector2 shootDirNormal = shootDir.normalized;
             gelbit.rigidbody2D.velocity = new Vector2(shootDirNormal.x * shootSpeed, (shootDirNormal.y * shootSpeed) + wobbleFactor);
