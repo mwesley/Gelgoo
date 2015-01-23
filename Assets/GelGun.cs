edfb2b10a3ee804375338de5a63bd972ff771de6
Marc Wesley <marcwesley99@googlemail.com>
2015-01-23 00:35:02 +0000

    Initial commit
    Lots of fun stuff! Start of a hopefully fun project

---
+++ Assets/GelGun.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GelGun : MonoBehaviour {
+
+    private Vector3 mousePos;
+    private float z;
+    public GameObject gelBit;
+    private Vector2 shootDir;
+    public float shootSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos.magnitude);
+        z = Mathf.Atan2((mousePos.y - transform.position.y), (mousePos.x - transform.position.x)) * Mathf.Rad2Deg;
+        transform.eulerAngles = new Vector3(0, 0, z);
+
+        ShootGel();
+
+	
+	}
+
+    private void ShootGel()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            float wobbleFactor = Random.Range(-0.5f, 0.5f);
+            shootDir = mousePos - transform.position;
+            GameObject go = Instantiate(gelBit, transform.position, Quaternion.identity) as GameObject;
+            go.rigidbody2D.velocity = new Vector2(shootDir.x * shootSpeed, (shootDir.y + wobbleFactor) * shootSpeed);
+        }
+    
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate()
+    {
+        InputMovement();
+    }
+
+    private void InputMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(x * moveSpeed, rigidbody2D.velocity.y);
+    }
+}
